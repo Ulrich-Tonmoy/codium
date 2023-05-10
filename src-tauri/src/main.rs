0ab82d31f2c7b572c007c121d1e4bbc11c1395b3
Ulrich <tonmoyulrich@gmail.com>
2023-05-10 16:36:19 +0600

    feat: added open_folder functionality

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,35 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod file;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn open_folder(folder_path: &str) -> String {
+    let files = file::read_directory(folder_path);
+    files
+}
+
+#[tauri::command]
+fn get_file_content(file_path: &str) -> String {
+    let content = file::read_file(file_path);
+    content
+}
+
+#[tauri::command]
+fn write_file(file_path: &str, content: &str) -> String {
+    file::write_file(file_path, content);
+    String::from("OK")
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, open_folder, get_file_content, write_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
